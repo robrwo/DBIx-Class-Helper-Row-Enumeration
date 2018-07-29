0d32ab69a44e38513930a51edad16728b89eefe0
Robert Rothenberg <rrwo@cpan.org>
2018-07-29 12:06:32 +0100

    Initial prototype (untested)

---
+++ lib/DBIx/Class/Helper/Row/Enumeration.pm
@@ -0,0 +1,66 @@
+package DBIx::Class::Helper::Row::Enumeration;
+
+# ABSTRACT: Add methods for emum values
+
+use v5.10;
+
+use strict;
+use warnings;
+
+use parent 'DBIx::Class::Core';
+
+use Ref::Util  ();
+use Sub::Quote ();
+
+our $VERSION = 'v0.1.0';
+
+sub add_columns {
+    my ( $self, @cols ) = @_;
+
+    $self->next::method(@cols);
+
+    my $class = Ref::Util::is_ref($self) || $self;
+
+    foreach my $col (@cols) {
+
+        next if ref $col;
+
+        $col =~ s/^\+//;
+
+        my $info = $self->column_info($col);
+
+        next unless $info->{data_type} eq 'enum';
+
+        my $handlers = $info->{extra}{handles} //= sub { "is_" . $_[0] };
+
+        next unless $handlers;
+
+        if ( Ref::Util::is_plain_coderef($handlers) ) {
+            $info->{extra}{handles} =
+              { map { $handlers->( $_, $col, $class ) => $_ }
+                  @{ $info->{extra}{list} } };
+            $handlers = $info->{extra}{handles};
+        }
+
+        die "handlers is not a hashref"
+          unless Ref::Util::is_plain_hashref($handlers);
+
+        my $acc = $info->{accessor} // $col;
+
+        foreach my $handler ( keys %$handlers ) {
+            my $value = $handlers->{$handler} or next;
+
+            my $method = "${class}::${handler}";
+
+            die "${method} is already defined" if $self->can($method);
+
+            Sub::Quote::quote_sub $method, qq{ \$_[0]->${acc} eq "${value}" };
+
+        }
+
+    }
+
+    return $self;
+}
+
+1;
